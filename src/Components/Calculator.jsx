459355c17b99491b5ecac2dbff1a7fefdc80290d
bdiniscia <bdiniscia@gmail.com>
2021-04-01 16:32:23 +0200

    Changing the formula function

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -9,26 +9,22 @@ const Calculator = () => {
     const [ unit, setUnit ] = useState(CELSIUS);
     const [ tempConverted, setTempConverted ] = useState(null);
     const [ tempToConvert, setTempToConvert ] = useState(null);
-    const [ notANumber, setNotANumber ] = useState(false);
     
     // Formula to process the conversion 
     const formula = (temp, unit) => {
-        let temperature = sanitize(temp);
+        const temperature = sanitize(temp);
         // If is NaN show the flag and get out of the function
         if (Number.isNaN(temperature)) {
-            return setNotANumber(true);
+            return '';
         }
         // Apply the correct formula
-        if(unit === CELSIUS) {
-            temperature = temperature * 9 / 5 + 32
-        } else if (unit === FAHRENHEIT) {
-            temperature = (temperature - 32) * 5 / 9;
-        }
+        const convertedTemperature = unit === CELSIUS ? temperature * 9 / 5 + 32 : (temperature - 32) * 5 / 9;
+
         // Check if it's a and integer,  if not show just 1 decimal place
-        if (Number.isInteger(temperature)) {
-            return temperature;
+        if (Number.isInteger(convertedTemperature)) {
+            return convertedTemperature;
         }
-        return temperature.toFixed(1);
+        return convertedTemperature.toFixed(1);
     }
 
     const sanitize = (input) => {
@@ -43,7 +39,6 @@ const Calculator = () => {
 
     // Handles the change in the input
     const handleChangeInput = (e) => {
-        setNotANumber(false);
         setTempToConvert(e.target.value);
         return setTempConverted(formula(e.target.value, unit));
     }
@@ -60,25 +55,25 @@ const Calculator = () => {
     }
 
     return (
-        <div className='calculator'>
-            <div className='data-entry'>
+        <div className={styles.calculator}>
+            <div className={styles.dataEntry}>
                 <div>
                     <input 
-                    className='input-temperature'
+                    className={styles.inputTemperature}
                     placeholder='Type your temperature' 
                     name='temperature'
-                    onChange = { e => handleChangeInput(e)}
+                    onChange = {handleChangeInput}
                     />
-                    <label className='units' for='temperature'> °{unit}</label>
+                    <label className={styles.units} for='temperature'> °{unit}</label>
                 </div>
-                {notANumber && <span className='not-a-number'>Please introduce a valid number</span>}
+                {tempConverted === '' && <span className={styles.notANumber}>Please introduce a valid number</span>}
             </div>
-            <img src={reverse} alt='Change units' onClick={e => clickChangeUnit(e)} className='reverse-btn'/>
-            <div className='result'>
-                <div className='result-number'>
+            <img src={reverse} alt='Change units' onClick={clickChangeUnit} className={styles.reverseBtn}/>
+            <div className={styles.result}>
+                <div className={styles.resultNumber}>
                     <span>{tempConverted}</span>
                 </div>
-                <span className='units'>{unit === CELSIUS ? '°F' : '°C'}</span>
+                <span className={styles.units}>{unit === CELSIUS ? '°F' : '°C'}</span>
             </div>
         </div>
     )
